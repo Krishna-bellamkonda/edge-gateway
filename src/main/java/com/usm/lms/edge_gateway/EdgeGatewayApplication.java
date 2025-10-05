cead13f3654413a969a38a9128fb340a2d1ec91b
Krishna-bellamkonda <kanthkrishna02@gmail.com>
2025-10-05 18:35:16 +0530

    NoCode Commit

---
+++ src/main/java/com/usm/lms/edge_gateway/EdgeGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.usm.lms.edge_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeGatewayApplication.class, args);
+	}
+
+}
